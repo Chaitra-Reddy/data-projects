6f392f1095983b143e48d6d6da6db2382d51b813
Chaitra Reddy <chaitrareddy1211@gmail.com>
2023-05-15 13:46:33 +0530

    added linkedin prok v2 and roller coaster proj

---
+++ LinkedInJobsProject_v2/README.md
@@ -0,0 +1,4 @@
+# LinkedIn Jobs Data Analysis Project (Version 2)
+
+- In this project, we'll be scraping the job postings data from LinkedIn website based on job title given in the "constants.py" file
+
---
+++ LinkedInJobsProject_v2/constants.py
@@ -0,0 +1,8 @@
+# JOBS_LIST_URL = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Data%2BAnalyst&location=India&geoId=102713980&trk=public_jobs_jobs-search-bar_search-submit&start={}'
+# JOB_DETAILS_URL = 'https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{}'
+
+
+JOBS_LIST_URL = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={}&location=India&geoId=102713980&trk=public_jobs_jobs-search-bar_search-submit&start={}'
+JOB_DETAILS_URL = 'https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{}'
+
+JOB_TITLE = 'Python'
